3d7adfe5d783f5809195e05dd591e7c4b660d1be
rifkialdi <rifkialdiansyah43@gmail.com>
2022-04-22 03:39:08 +0700

    commit at home

--- app/src/main/java/com/codepalace/light_sensor/MainActivity.kt
+++ app/src/main/java/com/codepalace/light_sensor/MainActivity.kt
@@ -6,39 +6,57 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
 import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
+    /* variable sensor dengan di lateinit */
     private lateinit var sensorManager: SensorManager
     private var brightness: Sensor? = null
+
+    /* memberi nama variabel dengan lateinit */
     private lateinit var text: TextView
     private lateinit var pb: CircularProgressBar
+    private lateinit var btnStart: Button
+    private lateinit var btnStop: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         text = findViewById(R.id.tv_text)
         pb = findViewById(R.id.circularProgressBar)
-
+        btnStart = findViewById(R.id.idbtn_start)
+        btnStop = findViewById(R.id.idbtn_stop)
 
         setUpSensorStuff()
+
+        btnStart.setOnClickListener {
+            sensorManager.registerListener(this, brightness, SensorManager.SENSOR_DELAY_NORMAL)
+            btnStart.isEnabled = false
+            btnStop.isEnabled = true
+        }
+
+        btnStop.setOnClickListener {
+            sensorManager.unregisterListener(this)
+            btnStart.isEnabled = true
+            btnStop.isEnabled = false
+            text.setText("Klik Tombol Start")
+        }
     }
 
     private fun setUpSensorStuff() {
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-
         brightness = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
-
-        sensorManager.registerListener(this, brightness, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
+    /* function override from SensorEventListener */
     override fun onSensorChanged(event: SensorEvent?) {
         if (event?.sensor?.type == Sensor.TYPE_LIGHT) {
             val light1 = event.values[0]
@@ -51,17 +69,25 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         return
     }
+    /**/
 
     private fun brightness(brightness: Float): String {
 
         return when (brightness.toInt()) {
-            0 -> "Pitch black"
-            in 1..10 -> "Dark"
-            in 11..50 -> "Grey"
+            0 -> "Gelap Gulita"
+            in 1..10 -> "Gelap"
+            in 11..50 -> "Lumayan"
             in 51..5000 -> "Normal"
-            in 5001..25000 -> "Incredibly bright"
-            else -> "This light will blind you"
+            in 5001..25000 -> "cerah"
+            else -> "cerah bangett"
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        btnStart.isEnabled = true
+        btnStop.isEnabled = false
     }
 
 }
